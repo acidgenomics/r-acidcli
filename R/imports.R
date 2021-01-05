a66a291cf04215e021a5afe41fda2417d559a5ba
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-05 14:17:26 -0500

    Work toward initial release

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,7 @@
 ## Defined in default packages: end (stats), text (graphics)
 
 #' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_alert_warning
-#'   cli_div cli_dl cli_end cli_h1 cli_h2 cli_h3 cli_text cli_ul cli_verbatim
+#'   cli_div cli_dl cli_end cli_h1 cli_h2 cli_h3 cli_ol cli_text cli_ul
+#'   cli_verbatim
 #' @importFrom crayon magenta
 NULL
