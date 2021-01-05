109d65950fc8b1d1bd1dc9bf6a0d4100c930df4c
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-05 14:12:27 -0500

    Improve lists

--- R/list.R
+++ R/list.R
@@ -1,14 +1,34 @@
-#' Unordered list
+#' Lists
 #'
-#' @export
+#' @name list
 #' @note Updated 2021-01-04.
 #'
 #' @param x `character(1)`.
 #'   Text to be displayed as list items.
 #'
 #' @examples
+#' dl(c(a = "AAA", b = "BBB"))
 #' ul(c("Item 1", "Item 2"))
+NULL
+
+
+
+#' @rdname list
+#' @export
+dl <- function(x) {
+    stopifnot(
+        is.character(x),
+        isTRUE(nzchar(names(x)))
+    )
+    cli_dl(x)
+}
+
+
+
+#' @rdname list
+#' @export
 ul <- function(x) {
+    stopifnot(is.character(x))
     indent <- 4L
     cli_div(theme = list(body = list("margin-left" = indent)))
     cli_ul(items = x)
