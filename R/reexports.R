d36d90cfafd44b91ac835f367d3331f169d28ef5
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-05 14:05:55 -0500

    Work toward initial release

---
+++ R/reexports.R
@@ -0,0 +1,85 @@
+#' @importFrom cli cli_alert
+#' @export
+cli::cli_alert
+
+#' @importFrom cli cli_alert_info
+#' @export
+cli::cli_alert_info
+
+#' @importFrom cli cli_alert_success
+#' @export
+cli::cli_alert_success
+
+#' @importFrom cli cli_alert_warning
+#' @export
+cli::cli_alert_warning
+
+#' @importFrom cli cli_div
+#' @export
+cli::cli_div
+
+#' @importFrom cli cli_dl
+#' @export
+cli::cli_dl
+
+#' @importFrom cli cli_end
+#' @export
+cli::cli_end
+
+#' @importFrom cli cli_h1
+#' @export
+cli::cli_h1
+
+#' @importFrom cli cli_h2
+#' @export
+cli::cli_h2
+
+#' @importFrom cli cli_h3
+#' @export
+cli::cli_h3
+
+#' @importFrom cli cli_text
+#' @export
+cli::cli_text
+
+#' @importFrom cli cli_ul
+#' @export
+cli::cli_ul
+
+#' @importFrom cli cli_verbatim
+#' @export
+cli::cli_verbatim
+
+
+
+#' @importFrom crayon blue
+#' @export
+crayon::blue
+
+#' @importFrom crayon bold
+#' @export
+crayon::bold
+
+#' @importFrom crayon cyan
+#' @export
+crayon::cyan
+
+#' @importFrom crayon green
+#' @export
+crayon::green
+
+#' @importFrom crayon magenta
+#' @export
+crayon::magenta
+
+#' @importFrom crayon red
+#' @export
+crayon::red
+
+#' @importFrom crayon underline
+#' @export
+crayon::underline
+
+#' @importFrom crayon yellow
+#' @export
+crayon::yellow
