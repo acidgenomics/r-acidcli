c858957a303f447273ef07dee5aaea2040f77028
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-05 14:24:34 -0500

    Initial version that passes build checks

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,7 @@
 ## Defined in default packages: end (stats), text (graphics)
 
 #' @importFrom cli cli_alert cli_alert_danger cli_alert_info cli_alert_success
-#'   cli_alert_warning cli_div cli_dl cli_end cli_h1 cli_h2 cli_h3 cli_ol
-#'   cli_text cli_ul cli_verbatim
+#'   cli_alert_warning cli_div cli_dl cli_end cli_ol cli_text cli_ul
+#'   cli_verbatim
 #' @importFrom crayon magenta
 NULL
