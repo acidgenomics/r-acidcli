1a4ebc364f45c499b034d5a6b4e9faeb6daac777
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-26 16:12:16 -0500

    Prepare initial stable(ish) release

--- R/package.R
+++ R/package.R
@@ -1,9 +1,11 @@
-## Defined in default packages: end (stats), text (graphics)
-
+#' AcidCLI
+#'
+#' Command line interface helpers for Acid Genomics packages.
+#'
 #' @importFrom cli cli_alert cli_alert_danger cli_alert_info cli_alert_success
 #'   cli_alert_warning cli_div cli_dl cli_end cli_ol cli_text cli_ul
 #'   cli_verbatim
 #' @importFrom crayon magenta
 #' @importFrom goalie assert isCharacter hasNames isInt isString
 #' @importFrom utils head
-NULL
+"_PACKAGE"
