b10a60b8f8649c2bf62a325b4d697c3a6697376f
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-13 14:34:40 -0400

    Add support for handling of `simpleCondition`

--- R/abort.R
+++ R/abort.R
@@ -1,16 +1,15 @@
 #' Signal an error, warning, or message with cli formatting
 #'
 #' @name abort
-#' @note Updated 2021-07-12.
+#' @note Updated 2021-08-13.
 #'
-#' @param x `character`.
+#' @param x `character` or `condition` (i.e. returned from `stop` or `warning`).
 #'
 #' @return Console output, with invisible return of `x` input.
 #'
 #' @seealso
-#' - [cli::cli_abort()].
-#' - [cli::cli_inform()].
-#' - [cli::cli_warn()].
+#' - [cli::cli_abort()], [cli::cli_inform()], [cli::cli_warn()].
+#' - `simpleCondition()`, `simpleError()`, `simpleWarning()`, `simpleMessage()`.
 #'
 #' @examples
 #' x <- "hello world"
@@ -25,11 +24,12 @@ NULL
 
 
 
-## NOTE Consider adding support for "i" and "x" named arguments here.
-
 #' @rdname abort
 #' @export
 abort <- function(x) {
+    if (is(x, "condition")) {
+        x <- x[["message"]]
+    }
     assert(isCharacter(x))
     x <- paste(x, collapse = "\n")
     cli_abort(x)
@@ -40,6 +40,9 @@ abort <- function(x) {
 #' @rdname abort
 #' @export
 inform <- function(x) {
+    if (is(x, "condition")) {
+        x <- x[["message"]]
+    }
     assert(isCharacter(x))
     lapply(X = x, FUN = cli_inform)
     invisible(x)
@@ -50,6 +53,9 @@ inform <- function(x) {
 #' @rdname abort
 #' @export
 warn <- function(x) {
+    if (is(x, "condition")) {
+        x <- x[["message"]]
+    }
     assert(isCharacter(x))
     lapply(X = x, FUN = cli_warn)
     invisible(x)
