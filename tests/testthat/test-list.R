1d69b3a3c635ef5f0a0949c667633340ad562db2
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-13 15:07:07 -0400

    Improve code coverage

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -1,1 +1,23 @@
 context("list")
+
+test_that("definition list", {
+    expect_message(
+        object = dl(c(
+            "aaa" = "bbb",
+            "ccc" = "ddd"
+        )),
+        regexp = "aaa"
+    )
+})
+
+test_that("ordered, unordered list", {
+    for (fun in list(
+        ol,
+        ul
+    )) {
+        expect_message(
+            object = fun(c("XXX", "YYY")),
+            regexp = "XXX"
+        )
+    }
+})
