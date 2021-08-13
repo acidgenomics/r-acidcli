e28ac7ad90c0bbd9133429c7342dc854f7a7e4bf
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-13 15:02:35 -0400

    Improve coverage

--- tests/testthat/test-alert.R
+++ tests/testthat/test-alert.R
@@ -1,1 +1,16 @@
 context("alert")
+
+test_that("alert", {
+    for (fun in list(
+        alert,
+        alertDanger,
+        alertInfo,
+        alertSuccess,
+        alertWarning
+    )) {
+        expect_message(
+            object = fun(c("XXX", "YYY")),
+            regexp = "XXX"
+        )
+    }
+})
