8d2baee415030807ed46fa10871b2537738948df
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-13 15:07:07 -0400

    Improve code coverage

--- tests/testthat/test-inline.R
+++ tests/testthat/test-inline.R
@@ -1,1 +1,11 @@
 context("inline")
+
+test_that("toInlineString", {
+    expect_identical(
+        object = toInlineString(
+            x = c("aaa", "bbb"),
+            class = "var"
+        ),
+        expected = "{.var aaa}, {.var bbb}"
+    )
+})
