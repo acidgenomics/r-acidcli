cd961b899386d85f34cd23f3f242440372f2f449
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 13:02:17 -0500

    Reorganize imports

--- R/package.R
+++ R/package.R
@@ -2,11 +2,4 @@
 #'
 #' Command line interface helpers for Acid Genomics packages.
 #'
-#' @importFrom cli cli_abort cli_alert cli_alert_danger cli_alert_info
-#'   cli_alert_success cli_alert_warning cli_div cli_dl cli_end cli_inform
-#'   cli_ol cli_text cli_ul cli_verbatim cli_warn
-#' @importFrom crayon magenta
-#' @importFrom goalie assert isCharacter hasNames isInt isPositive isString
-#' @importFrom methods is
-#' @importFrom utils head
 "_PACKAGE"
