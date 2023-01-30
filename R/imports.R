b8cb46a8e95e8048b2d60e7030ab77bffebd64d0
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-30 09:30:35 -0500

    Ensure rlang gets installed

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,9 @@
 #' @importFrom cli cli_abort cli_alert cli_alert_danger cli_alert_info
 #' cli_alert_success cli_alert_warning cli_div cli_dl cli_end cli_inform
-#' cli_ol cli_text cli_ul cli_verbatim cli_warn is_utf8_output
+#' cli_ol cli_text cli_ul cli_verbatim cli_warn format_message is_utf8_output
 #' @importFrom crayon cyan magenta
 #' @importFrom goalie assert isCharacter hasNames isInt isPositive isString
 #' @importFrom methods is
+#' @importFrom rlang abort inform warn
 #' @importFrom utils head
 NULL
