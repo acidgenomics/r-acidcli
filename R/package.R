a788bc249f9815af675e31cb55a39451587621ec
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 09:22:16 -0400

    Consolidate documentation

--- R/package.R
+++ R/package.R
@@ -3,3 +3,17 @@
 #' Command line interface helpers for Acid Genomics packages.
 #'
 "_PACKAGE"
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom cli cli_abort cli_alert cli_alert_danger cli_alert_info
+#' cli_alert_success cli_alert_warning cli_div cli_dl cli_end cli_inform
+#' cli_ol cli_text cli_ul cli_verbatim cli_warn format_message is_utf8_output
+#' @importFrom crayon cyan magenta
+#' @importFrom goalie assert isCharacter hasNames isInt isPositive isString
+#' @importFrom methods is
+#' @importFrom rlang abort inform warn
+#' @importFrom utils head
+NULL
